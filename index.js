798978f7ef6e81c544b1c43c40c133536df9a047
Bryan <aliceabryan1996@gmail.com>
2023-07-28 01:25:23 -0400

    test broken. My funtion does what the instructions ask for but i am not sure why the last test is still failing.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function  writeCards(array, event){
+    let myArray = []
+    for (let index = 0; index < array.length; index++){
+        myArray.push(`Thank you, ${array[index]}, for the wonderful ${event} gift!`)
+    }
+    return myArray
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(`${number}`)
+        number--
+    }
+}
